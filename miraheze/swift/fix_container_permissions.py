603d01c4cfac86bc9bc3125f094c30a8d2e8bc02
paladox <paladox@users.noreply.github.com>
2024-02-09 22:31:32 +0000

    Fix indentation in fix_container_permissions.py

--- miraheze/swift/fix_container_permissions.py
+++ miraheze/swift/fix_container_permissions.py
@@ -4,8 +4,8 @@ import subprocess
 import sys
 
 if len(sys.argv) != 2:
-        print("Please use in form of 'python fix_containers.py foowiki'")
-        sys.exit(1)
+    print("Please use in form of 'python fix_containers.py foowiki'")
+    sys.exit(1)
 
 wiki = sys.argv[1]
 
@@ -13,6 +13,6 @@ out = subprocess.run(['sudo', '-u', 'www-data', 'php', '/srv/mediawiki/1.41/main
 
 matches = re.findall(r"Making sure 'mwstore:\/\/miraheze-swift\/([^']+)' [^\n]+\.failed\.", out.stdout)
 for match in matches:
-        os.system(f"swift post --read-acl 'mw:media' --write-acl 'mw:media' miraheze-{wiki}-{match}")
+    os.system(f"swift post --read-acl 'mw:media' --write-acl 'mw:media' miraheze-{wiki}-{match}")
 
-os.system(f"sudo -u www-data php /srv/mediawiki/1.41/maintenance/run.php /srv/mediawiki/1.41/extensions/CreateWiki/maintenance/setContainersAccess.php --wiki {wiki}")
+os.system(f'sudo -u www-data php /srv/mediawiki/1.41/maintenance/run.php /srv/mediawiki/1.41/extensions/CreateWiki/maintenance/setContainersAccess.php --wiki {wiki}')
