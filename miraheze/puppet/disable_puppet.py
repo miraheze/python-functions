d2f7db15261d4a61f49da290944e4d75668c890f
RhinosF1 <rhinosf1@wikitide.org>
2024-05-18 17:05:31 +0100

    Update disable_puppet.py

--- miraheze/puppet/disable_puppet.py
+++ miraheze/puppet/disable_puppet.py
@@ -4,7 +4,7 @@ from os import system
 import sys
 
 
-def disable_puppet()-> None:
+def disable_puppet() -> None:
     system(f'logsalmsg Disabling puppet for {sys.argv[1]}')
     system('sudo puppet agent -tv')
     system(f'sudo puppet agent --disable "{sys.argv[1]}"')
