76c084b6099fd302ac02c9ba3cc3ed708321b40b
RhinosF1 <rhinosf1@wikitide.org>
2024-05-18 14:46:05 +0100

    Apply suggestions from code review
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- miraheze/swift/fix_container_permissions.py
+++ miraheze/swift/fix_container_permissions.py
@@ -8,13 +8,13 @@ def fix_container_perms(wiki: str) -> None:
     out = subprocess.run(['sudo', '-u', 'www-data', 'php', '/srv/mediawiki/1.41/maintenance/run.php', '/srv/mediawiki/1.41/extensions/CreateWiki/maintenance/setContainersAccess.php', '--wiki', wiki], capture_output=True, text=True)
     matches = re.findall(r"Making sure 'mwstore:\/\/miraheze-swift\/([^']+)' [^\n]+\.failed\.", out.stdout)
     for match in matches:
-        os.system(f"swift post --read-acl 'mw:media' --write-acl 'mw:media' miraheze-{wiki}-{match}")
+        subprocess.run(['swift', 'post', '--read-acl', 'mw:media', '--write-acl', 'mw:media', f'miraheze-{wiki}-{match}'], check=True)
 
     os.system(f'sudo -u www-data php /srv/mediawiki/1.41/maintenance/run.php /srv/mediawiki/1.41/extensions/CreateWiki/maintenance/setContainersAccess.php --wiki {wiki}')
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description='Executes the commands needed to reset wikis')
+    parser = argparse.ArgumentParser(description='Fix container permissions for a specified wiki')
     parser.add_argument('--wiki', required=True, help='wiki database name')
 
     args = parser.parse_args()
