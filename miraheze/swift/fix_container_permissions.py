168c2ffffd3f0141a7a7b3d8b5a3d5f5f9d1b8a8
RhinosF1 <rhinosf1@wikitide.org>
2024-05-18 14:17:37 +0100

    Update fix_container_permissions.py

--- miraheze/swift/fix_container_permissions.py
+++ miraheze/swift/fix_container_permissions.py
@@ -2,17 +2,25 @@ import os
 import re
 import subprocess
 import sys
+import argparse
 
-if len(sys.argv) != 2:
-    print("Please use in form of 'python fix_containers.py foowiki'")
-    sys.exit(1)
 
-wiki = sys.argv[1]
+def fix_container_perms(wiki: str) -> None:
+    out = subprocess.run(['sudo', '-u', 'www-data', 'php', '/srv/mediawiki/1.41/maintenance/run.php', '/srv/mediawiki/1.41/extensions/CreateWiki/maintenance/setContainersAccess.php', '--wiki', wiki], capture_output=True, text=True)
+    matches = re.findall(r"Making sure 'mwstore:\/\/miraheze-swift\/([^']+)' [^\n]+\.failed\.", out.stdout)
+    for match in matches:
+        os.system(f"swift post --read-acl 'mw:media' --write-acl 'mw:media' miraheze-{wiki}-{match}")
 
-out = subprocess.run(['sudo', '-u', 'www-data', 'php', '/srv/mediawiki/1.41/maintenance/run.php', '/srv/mediawiki/1.41/extensions/CreateWiki/maintenance/setContainersAccess.php', '--wiki', wiki], capture_output=True, text=True)
+    os.system(f'sudo -u www-data php /srv/mediawiki/1.41/maintenance/run.php /srv/mediawiki/1.41/extensions/CreateWiki/maintenance/setContainersAccess.php --wiki {wiki}')
 
-matches = re.findall(r"Making sure 'mwstore:\/\/miraheze-swift\/([^']+)' [^\n]+\.failed\.", out.stdout)
-for match in matches:
-    os.system(f"swift post --read-acl 'mw:media' --write-acl 'mw:media' miraheze-{wiki}-{match}")
 
-os.system(f'sudo -u www-data php /srv/mediawiki/1.41/maintenance/run.php /srv/mediawiki/1.41/extensions/CreateWiki/maintenance/setContainersAccess.php --wiki {wiki}')
+def main() -> None:
+    parser = argparse.ArgumentParser(description='Executes the commands needed to reset wikis')
+    parser.add_argument('--wiki', required=True, help='wiki database name')
+
+    args = parser.parse_args()
+    fix_container_perms(args.wiki)
+
+
+if __name__ == '__main__':
+    main()
