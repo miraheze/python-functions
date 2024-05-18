b6f0d70b052a42e74db993fe0788ef6dcb839d67
RhinosF1 <rhinosf1@wikitide.org>
2024-05-18 16:19:33 +0100

    Update cycle_puppet.py

--- miraheze/mediawiki/cycle_puppet.py
+++ miraheze/mediawiki/cycle_puppet.py
@@ -1,7 +1,5 @@
 #! /usr/bin/python3
 
-from os import system
-import sys
 from miraheze.mediawiki import disable_puppet, enable_puppet
 disable_puppet.disable_puppet()
 input('press enter to re-enable puppet')
