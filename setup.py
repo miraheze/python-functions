7a45b29100ed44d6612ae14ca05614aab36b0acc
RhinosF1 <rhinosf1@wikitide.org>
2024-05-18 13:06:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='Miraheze_PyUtils',
     version=VERSION,
     description='Python Utilities for Miraheze',
-    long_description=readme
+    long_description=readme,
     long_description_content_type='text/markdown',  # This is important!
     author='RhinosF1',
     author_email='rhinosf1@wikitide.org',
