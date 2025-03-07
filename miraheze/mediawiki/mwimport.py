936dfa40500f5058c6b13b493318da4f229f166a
BlankEclair <blankeclair@disroot.org>
2025-03-07 21:16:14 +1100

    Use single quotes instead of double quotes

--- miraheze/mediawiki/mwimport.py
+++ miraheze/mediawiki/mwimport.py
@@ -7,34 +7,34 @@ import sys
 
 
 def parse_args(input_args: list | None = None, check_paths: bool = True) -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description="A script to automate manual wiki imports")
+    parser = argparse.ArgumentParser(description='A script to automate manual wiki imports')
     parser.add_argument(
-        "--no-log", dest="nolog", action="store_true",
-        help="Whether or not to disable logging to the server admin log",
+        '--no-log', dest='nolog', action='store_true',
+        help='Whether or not to disable logging to the server admin log',
     )
     parser.add_argument(
-        "--confirm", "--yes", "-y", dest="confirm", action="store_true",
-        help="Whether or not to skip the initial confirmation prompt",
+        '--confirm', '--yes', '-y', dest='confirm', action='store_true',
+        help='Whether or not to skip the initial confirmation prompt',
     )
-    parser.add_argument("--version", help="MediaWiki version to use (automatically detected if not passed)")
-    parser.add_argument("--xml", help="XML file to import")
-    parser.add_argument("--username-prefix", help="Interwiki prefix for importing XML")
-    parser.add_argument("--images", help="Directory of images to import")
+    parser.add_argument('--version', help='MediaWiki version to use (automatically detected if not passed)')
+    parser.add_argument('--xml', help='XML file to import')
+    parser.add_argument('--username-prefix', help='Interwiki prefix for importing XML')
+    parser.add_argument('--images', help='Directory of images to import')
     parser.add_argument(
-        "--images-comment", help="The comment passed to importImages.php"
-        " (example: 'Importing images from https://example.com ([[phorge:T1234|T1234]])')",
+        '--images-comment', help='The comment passed to importImages.php'
+        ' (example: "Importing images from https://example.com ([[phorge:T1234|T1234]])")',
     )
     parser.add_argument(
-        "--search-recursively", action="store_true",
-        help="Whether or not to pass --search-recursively (check files in subdirectories) to importImages.php",
+        '--search-recursively', action='store_true',
+        help='Whether or not to pass --search-recursively (check files in subdirectories) to importImages.php',
     )
-    parser.add_argument("wiki", help="Database name of the wiki to import to")
+    parser.add_argument('wiki', help='Database name of the wiki to import to')
 
     args = parser.parse_args(input_args)
     if not args.xml and not args.images:
-        raise ValueError("--xml and/or --images must be passed")
+        raise ValueError('--xml and/or --images must be passed')
     if args.images and not args.images_comment:
-        raise ValueError("--images-comment must be passed when importing images")
+        raise ValueError('--images-comment must be passed when importing images')
 
     # This is honestly only for unit testing as I can't really think of a reason
     # to disable this check in production
@@ -42,23 +42,23 @@ def parse_args(input_args: list | None = None, check_paths: bool = True) -> argp
         # This is not meant to be comprehensive, but just to make sure that someone
         # doesn't typo a path or so
         if args.xml and not os.path.exists(args.xml):
-            raise ValueError(f"Cannot find XML to import: {repr(args.xml)}")
+            raise ValueError(f'Cannot find XML to import: {repr(args.xml)}')
         if args.images and not os.path.exists(args.images):
-            raise ValueError(f"Cannot find images to import: {repr(args.images)}")
+            raise ValueError(f'Cannot find images to import: {repr(args.images)}')
 
     return args
 
 
 def log(message: str):
     subprocess.run(
-        ["/usr/local/bin/logsalmsg", message],
+        ['/usr/local/bin/logsalmsg', message],
         check=True,
     )
 
 
 def get_version(wiki: str) -> str:
     return subprocess.run(
-        ["/usr/local/bin/getMWVersion", wiki],
+        ['/usr/local/bin/getMWVersion', wiki],
         stdout=subprocess.PIPE,
         check=True,
         text=True,
@@ -69,40 +69,40 @@ def get_scripts(args: argparse.Namespace) -> list[list[str]]:
     scripts = []
 
     if args.xml:
-        script = ["importDump", "--no-updates"]
+        script = ['importDump', '--no-updates']
         if args.username_prefix:
-            script.append(f"--username-prefix={args.username_prefix}")
-        script.extend(["--", args.xml])
+            script.append(f'--username-prefix={args.username_prefix}')
+        script.extend(['--', args.xml])
         scripts.append(script)
 
     if args.images:
-        script = ["importImages", f"--comment={args.images_comment}"]
+        script = ['importImages', f'--comment={args.images_comment}']
         if args.search_recursively:
-            script.append("--search-recursively")
-        script.extend(["--", args.images])
+            script.append('--search-recursively')
+        script.extend(['--', args.images])
         scripts.append(script)
 
     if args.xml:
-        scripts.append(["rebuildall"])
-        scripts.append(["initEditCount"])
+        scripts.append(['rebuildall'])
+        scripts.append(['initEditCount'])
 
-    scripts.append(["initSiteStats", "--update"])
+    scripts.append(['initSiteStats', '--update'])
 
     return scripts
 
 
 def run_scripts(args: argparse.Namespace, scripts: list[list[str]]) -> int:
     for script in scripts:
-        print(f"Running {shlex.join(script)}")
+        print(f'Running {shlex.join(script)}')
         if not args.nolog:
-            print("Logging execution...")
+            print('Logging execution...')
             log(shlex.join(script))
 
         proc = subprocess.run(script)
 
         if not args.nolog:
-            print("Logging execution end...")
-            log(f"{shlex.join(script)} (END - exit={proc.returncode})")
+            print('Logging execution end...')
+            log(f'{shlex.join(script)} (END - exit={proc.returncode})')
 
         if proc.returncode:
             return proc.returncode
@@ -114,34 +114,34 @@ def run():
     try:
         args = parse_args()
     except ValueError as e:
-        print(f"{type(e).__name__}: {e}", file=sys.stderr)
+        print(f'{type(e).__name__}: {e}', file=sys.stderr)
         return 1
 
     version = args.version or get_version(args.wiki)
 
     scripts = get_scripts(args)
     scripts = [
-        ["sudo", "-u", "www-data", "php", f"/srv/mediawiki/{version}/maintenance/run.php", f"--wiki={args.wiki}", *script] for script in scripts
+        ['sudo', '-u', 'www-data', 'php', f'/srv/mediawiki/{version}/maintenance/run.php', f'--wiki={args.wiki}', *script] for script in scripts
     ]
 
-    print("Will run:")
+    print('Will run:')
     for script in scripts:
-        print(f"* {shlex.join(script)}")
-    if not args.confirm and input("Type 'Y' to confirm: ").upper() != "Y":
+        print(f'* {shlex.join(script)}')
+    if not args.confirm and input("Type 'Y' to confirm: ").upper() != 'Y':
         return 1
 
     if not args.nolog:
-        print("Logging start...")
-        log(f"Starting import for {args.wiki} (XML: {args.xml}; Images: {args.images})")
+        print('Logging start...')
+        log(f'Starting import for {args.wiki} (XML: {args.xml}; Images: {args.images})')
 
     return_code = run_scripts(args, scripts)
 
     if not args.nolog:
-        print("Logging end...")
-        log(f"Finished import for {args.wiki} (XML: {args.xml}; Images: {args.images}) (END - exit={return_code})")
+        print('Logging end...')
+        log(f'Finished import for {args.wiki} (XML: {args.xml}; Images: {args.images}) (END - exit={return_code})')
 
     return return_code
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(run())
--- tests/test_mwimport.py
+++ tests/test_mwimport.py
@@ -4,125 +4,125 @@ from miraheze.mediawiki import mwimport
 
 def test_parse_args_xml_images():
     args = mwimport.parse_args([
-        "--xml=dump.xml",
-        "--images=images",
-        "--images-comment=Importing from https://example.com",
-        "examplewiki",
+        '--xml=dump.xml',
+        '--images=images',
+        '--images-comment=Importing from https://example.com',
+        'examplewiki',
     ], False)
-    assert args.xml == "dump.xml"
-    assert args.images == "images"
-    assert args.images_comment == "Importing from https://example.com"
-    assert args.wiki == "examplewiki"
+    assert args.xml == 'dump.xml'
+    assert args.images == 'images'
+    assert args.images_comment == 'Importing from https://example.com'
+    assert args.wiki == 'examplewiki'
 
 
 def test_parse_args_images_recursively():
     args = mwimport.parse_args([
-        "--images=images",
-        "--search-recursively",
-        "--images-comment=Importing from https://example.com",
-        "examplewiki",
+        '--images=images',
+        '--search-recursively',
+        '--images-comment=Importing from https://example.com',
+        'examplewiki',
     ], False)
-    assert args.images == "images"
+    assert args.images == 'images'
     assert args.search_recursively
-    assert args.images_comment == "Importing from https://example.com"
-    assert args.wiki == "examplewiki"
+    assert args.images_comment == 'Importing from https://example.com'
+    assert args.wiki == 'examplewiki'
 
 
 def test_parse_args_no_log_and_confirm_and_version():
     args = mwimport.parse_args([
-        "--no-log",
-        "--confirm",
-        "--version=0.42",
-        "--xml=dump.xml",
-        "examplewiki",
+        '--no-log',
+        '--confirm',
+        '--version=0.42',
+        '--xml=dump.xml',
+        'examplewiki',
     ], False)
     assert args.nolog
     assert args.confirm
-    assert args.version == "0.42"
+    assert args.version == '0.42'
 
 
 def test_parse_args_username_prefix():
     args = mwimport.parse_args([
-        "--xml=dump.xml",
-        "--username-prefix=w",
-        "examplewiki",
+        '--xml=dump.xml',
+        '--username-prefix=w',
+        'examplewiki',
     ], False)
-    assert args.username_prefix == "w"
+    assert args.username_prefix == 'w'
 
 
 def test_parse_args_need_xml_or_images():
-    with pytest.raises(ValueError, match="--xml and/or --images must be passed"):
+    with pytest.raises(ValueError, match='--xml and/or --images must be passed'):
         mwimport.parse_args([
-            "examplewiki",
+            'examplewiki',
         ])
 
 
 def test_parse_args_images_need_comment():
-    with pytest.raises(ValueError, match="--images-comment must be passed when importing images"):
+    with pytest.raises(ValueError, match='--images-comment must be passed when importing images'):
         mwimport.parse_args([
-            "--images=images",
-            "examplewiki",
+            '--images=images',
+            'examplewiki',
         ])
 
 
 def test_parse_args_missing_xml():
-    with pytest.raises(ValueError, match="Cannot find XML to import: '/dev/no xml'"):
+    with pytest.raises(ValueError, match='Cannot find XML to import: '/dev/no xml''):
         mwimport.parse_args([
-            "--xml=/dev/no xml",
-            "examplewiki",
+            '--xml=/dev/no xml',
+            'examplewiki',
         ])
 
 
 def test_parse_args_missing_images():
-    with pytest.raises(ValueError, match="Cannot find images to import: '/dev/no images'"):
+    with pytest.raises(ValueError, match='Cannot find images to import: '/dev/no images''):
         mwimport.parse_args([
-            "--images=/dev/no images",
-            "--images-comment=Importing from https://example.com",
-            "examplewiki",
+            '--images=/dev/no images',
+            '--images-comment=Importing from https://example.com',
+            'examplewiki',
         ])
 
 
 def test_get_scripts_xml_images():
     args = mwimport.parse_args([
-        "--xml=dump.xml",
-        "--images=images",
-        "--images-comment=Importing from https://example.com",
-        "examplewiki",
+        '--xml=dump.xml',
+        '--images=images',
+        '--images-comment=Importing from https://example.com',
+        'examplewiki',
     ], False)
     scripts = mwimport.get_scripts(args)
     assert scripts == [
-        ["importDump", "--no-updates", "--", "dump.xml"],
-        ["importImages", "--comment=Importing from https://example.com", "--", "images"],
-        ["rebuildall"],
-        ["initEditCount"],
-        ["initSiteStats", "--update"],
+        ['importDump', '--no-updates', '--', 'dump.xml'],
+        ['importImages', '--comment=Importing from https://example.com', '--', 'images'],
+        ['rebuildall'],
+        ['initEditCount'],
+        ['initSiteStats', '--update'],
     ]
 
 
 def test_get_scripts_username_prefix():
     args = mwimport.parse_args([
-        "--xml=dump.xml",
-        "--username-prefix=w",
-        "examplewiki",
+        '--xml=dump.xml',
+        '--username-prefix=w',
+        'examplewiki',
     ], False)
     scripts = mwimport.get_scripts(args)
     assert scripts == [
-        ["importDump", "--no-updates", "--username-prefix=w", "--", "dump.xml"],
-        ["rebuildall"],
-        ["initEditCount"],
-        ["initSiteStats", "--update"],
+        ['importDump', '--no-updates', '--username-prefix=w', '--', 'dump.xml'],
+        ['rebuildall'],
+        ['initEditCount'],
+        ['initSiteStats', '--update'],
     ]
 
 
 def test_get_scripts_search_recursively():
     args = mwimport.parse_args([
-        "--images=images",
-        "--images-comment=Importing from https://example.com",
-        "--search-recursively",
-        "examplewiki",
+        '--images=images',
+        '--images-comment=Importing from https://example.com',
+        '--search-recursively',
+        'examplewiki',
     ], False)
     scripts = mwimport.get_scripts(args)
     assert scripts == [
-        ["importImages", "--comment=Importing from https://example.com", "--search-recursively", "--", "images"],
-        ["initSiteStats", "--update"],
+        ['importImages', '--comment=Importing from https://example.com', '--search-recursively', '--', 'images'],
+        ['initSiteStats', '--update'],
     ]
