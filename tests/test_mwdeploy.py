0a0f8702c0a3ef778986b5401697c22598c36f16
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-08-10 13:21:40 -0600

    Replace RequestSSL with RequestCustomDomain and remove YouTube and SpriteSheet

--- tests/test_mwdeploy.py
+++ tests/test_mwdeploy.py
@@ -277,7 +277,7 @@ def test_UpgradePackAction():
     parser.add_argument('--versions', action='store', default=None)
     parser.add_argument('--upgrade-pack', action=UpgradePackAction)
     namespace = parser.parse_args(['--upgrade-pack', 'wikitide'])
-    assert namespace.upgrade_extensions == ['CreateWiki', 'DataDump', 'GlobalNewFiles', 'ImportDump', 'IncidentReporting', 'ManageWiki', 'MatomoAnalytics', 'MirahezeMagic', 'PDFEmbed', 'RemovePII', 'RequestSSL', 'RottenLinks', 'SpriteSheet', 'WikiDiscover', 'YouTube']
+    assert namespace.upgrade_extensions == ['CreateWiki', 'DataDump', 'GlobalNewFiles', 'ImportDump', 'IncidentReporting', 'ManageWiki', 'MatomoAnalytics', 'MirahezeMagic', 'PDFEmbed', 'RemovePII', 'RequestCustomDomain', 'RottenLinks', 'WikiDiscover']
 
 
 def test_LangAction():
